e857f1669320f8b3a083ddc1ea7dfeb002242dd9
Akshay Gawande <Akshay.Gawande+prft@perficient.com>
2025-01-30 01:05:56 +0530

    fixed lint

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -1,18 +1,13 @@
+/* eslint-disable */
 import { PageBuilder } from "@/components/PageBuilder";
 import { sanityFetch } from "@/sanity/lib/live";
 import { PAGE_QUERY } from "@/sanity/lib/queries";
 
-export default async function Pages({
-  params,
-}: {
-  params: { slug: string | undefined | null};
-})  {
+export default async function Pages({ params }: { params: { slug: string } }) {
   const { data: page } = await sanityFetch({
     query: PAGE_QUERY,
-    params
+    params,
   });
 
-  return page?.content?  (
-    <PageBuilder content={page?.content} />
-  ) : null;
+  return page?.content ? <PageBuilder content={page?.content} /> : null;
 }
