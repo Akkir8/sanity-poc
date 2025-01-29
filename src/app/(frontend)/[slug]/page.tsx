648f5cf73d50a378d7b543b18c0d12a96faae2cb
Akshay Gawande <Akshay.Gawande+prft@perficient.com>
2025-01-29 19:06:04 +0530

    commit

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -5,7 +5,7 @@ import { PAGE_QUERY } from "@/sanity/lib/queries";
 export default async function Pages({
   params,
 }: {
-  params: { slug: string };
+  params: { slug: string | undefined | null};
 })  {
   const { data: page } = await sanityFetch({
     query: PAGE_QUERY,
